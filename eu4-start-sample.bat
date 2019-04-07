e22cb6326fecf1f5d44858a85c7532ebde167370
FUN <fun.public.address@gmail.com>
2019-04-07 13:06:11 +0900

    add sample settings

--- eu4-start-sample.bat
+++ eu4-start-sample.bat
@@ -3,8 +3,7 @@
 set EU4PATH1=%HOMEDRIVE%%HOMEPATH%\Documents\Paradox Interactive\Europa Universalis IV
 set EU4PATH2=C:\Program Files (x86)\Steam\steamapps\common\Europa Universalis IV
 
-eu4-settings-switcher.py "%EU4PATH1%" --dlc --load settings\DLC.txt
-eu4-settings-switcher.py "%EU4PATH1%" --mod --load settings\MOD.txt
+eu4-settings-switcher.py "%EU4PATH1%" --mod --load settings\JPMOD-sample.txt
 
 cd "%EU4PATH2%"
 start eu4.exe
